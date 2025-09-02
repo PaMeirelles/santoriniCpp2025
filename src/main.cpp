90570325f7f3b49a5f8a6328ec6087655c784d57
rafael <rafaelpmeirelles@gmail.com>
2025-09-02 14:45:26 -0300

    faltou um detalhe em artemis x athena

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,16 @@
 
 
 int main() {
-    // std::ios_base::sync_with_stdio(false);
-    // std::cin.tie(NULL);
-    //
-    // Santorini::SantoriniEngine engine;
-    // engine.run();
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
 
-    auto pos = "0N1N0N0N0N0N1N1G0N0N0N0G0N0N0N0N1B0B0N0N0N2N1N0N0N1241";
-    Santorini::Board board = Santorini::Board(pos);
-    Santorini::TranspositionTable tt = Santorini::TranspositionTable();
-    auto mv = Santorini::get_best_move(board, 1000, tt);
-    cout << mv->to_text() << endl;
-    return 0;
+    Santorini::SantoriniEngine engine;
+    engine.run();
+
+    // auto pos = "1N1G0N0N0N0N0N0G0N0N0N0N0B0N0N0B1N0N0N0N0N0N0N0N0N1211";
+    // Santorini::Board board = Santorini::Board(pos);
+    // Santorini::TranspositionTable tt = Santorini::TranspositionTable();
+    // auto mv = Santorini::get_best_move(board, 1000, tt);
+    // cout << mv->to_text() << endl;
+    // return 0;
 }
