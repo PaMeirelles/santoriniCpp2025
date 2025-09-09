720758cafe19afaa16ea69e6796925b2efb9764c
rafael <rafaelpmeirelles@gmail.com>
2025-09-08 21:21:47 -0300

    5.5.9: this has to be the same as 5.5.3

--- src/search.h
+++ src/search.h
@@ -57,13 +57,13 @@ inline int evaluate(const Board& board) {
 constexpr std::array<std::array<int, 4>, 4> HEIGHT_SCORING =
     {{
         {
-            {0, 1, 0, 0}
+            {0, 1, 3, 0}
         },
         {
-            {-1, 0, 2, 0}
+            {-1, 0, 2, 4}
         },
         {
-            {-2, -1, 0, 3}
+            {-2, -1, 0, 4}
         },
         {
             {-1, 0, 2, 0}
